247b44b642c3e2eef5ccbb3c674f5c0c1a4df3e3
axxydr1xed <axxydr1xed@gmail.com>
2025-12-15 22:44:38 +0300

    WordCombiner.py
    The script itself. Why would you even download this, gee I wonder?

--- WordCombiner.py
+++ WordCombiner.py
@@ -1,3 +1,5 @@
 word1 = input("Type the first word you want to combine. ")
 word2 = input("Type the first word you want to combine. ")
 result = print(word1 + word2)
+
+# A comment that does nothing but I add this to update the file's commit thing.
